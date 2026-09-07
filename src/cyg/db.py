0a1f9b114ba7729709a71cf677cc47006fe7f906
smpouli <156841750+smpouli@users.noreply.github.com>
2026-09-07 19:11:10 +0200

    Update db.py

--- src/cyg/db.py
+++ src/cyg/db.py
@@ -5,17 +5,17 @@ import os, gzip, shutil
 import sqlite3
 
 # database checking and downloading
-path_to_installed_database=Path('src/cyg/data/cygnet.db')
+path_to_installed_database= Path(__file__).parent/"cyg_data"/"cygnet.db"
 
 if path_to_installed_database.exists():   
     # connexion to the database
-    conn = sqlite3.connect('src/cyg/data/cygnet.db')
+    conn = sqlite3.connect(path_to_installed_database)
     Database = conn.cursor()
 else:    
     download_url = "https://github.com/rowanhm/cygnet/releases/latest/download/cygnet.db.gz"
     
-    gz_file = Path("src/cyg/data/cygnet.db.gz")
-    output_file = Path("src/cyg/data/cygnet.db")
+    gz_file = Path(__file__).parent/"cyg_data"/"cygnet.db.gz"
+    
   
     
     try:
@@ -26,17 +26,17 @@ else:
         print ("Cygnet database successfully downloaded.")
         # Decompress
         with gzip.open(gz_file, "rb") as gz:
-            with open(output_file, "wb") as output:
+            with open(path_to_installed_database, "wb") as output:
                 shutil.copyfileobj(gz, output)
 
         os.remove(gz_file)
         print("Cygnet database successfully extracted and stored.")
         
         # connexion to the database
-        conn = sqlite3.connect('src/cyg/data/cygnet.db')
+        conn = sqlite3.connect(path_to_installed_database)
         Database = conn.cursor()
     except:
-        print ("An error occurred, please try again or look at the help file in the data folder.")
+        print ("An error occurred, please try again or look at the help file in the cyg_data folder.")
 
        
 
